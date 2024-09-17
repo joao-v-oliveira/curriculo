acade3e5205b1b9b56cf3887517530a858acd9c7
João Vitor <joao.vitor@icondev.com.br>
2024-09-16 21:32:13 -0300

    Ajustes do dia 16092024 V1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CurriculoComponent } from './curriculo/curriculo.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/meu_curriculo',
+    pathMatch: 'full'
+  },
+  {
+    path: "meu_curriculo",
+    component: CurriculoComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CurriculoComponent } from './curriculo/curriculo.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [	
+    AppComponent,
+      CurriculoComponent
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule
