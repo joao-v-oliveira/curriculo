140ace0fbf8acf7b4e9a1fa27812956a32d5ef2b
João Vitor <joao.vitor@icondev.com.br>
2024-09-16 23:33:48 -0300

    Ajustes do dia 16092024 V2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CurriculoComponent } from './curriculo/curriculo.component';
 
+import { ButtonModule } from 'primeng/button';
+
 @NgModule({
-  declarations: [	
+  declarations: [
     AppComponent,
       CurriculoComponent
    ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    ButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/curriculo/curriculo.component.ts
+++ src/app/curriculo/curriculo.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CurriculoComponent implements OnInit {
 
+  public value = 0;
+
   constructor() { }
 
   ngOnInit() {
